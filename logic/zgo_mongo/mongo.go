8618114ef7676ca4e48383dda2d2b09e0be06e2d
zhuhonglei <zhuhonglei@zhuge.com>
2019-02-18 19:44:46 +0800

    Merge branch 'master' of https://git.zhugefang.com/gocore/zgo

--- logic/zgo_mongo/mongo.go
+++ logic/zgo_mongo/mongo.go
@@ -2,7 +2,7 @@ package zgo_mongo
 
 import (
 	"context"
-	"git.zhugefang.com/gocore/zgo.git/models/zgo_models_mongo"
+	"git.zhugefang.com/gocore/zgo.git/dbs/zgo_db_mongo"
 	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
 )
@@ -16,22 +16,22 @@ func NewMongo() *Mongo {
 
 func (m *Mongo) GetClientChan(name string) chan *mgo.Session {
 	//name用来查找对应的库
-	return zgo_models_mongo.MongoClientChan()
+	return zgo_db_mongo.MongoClientChan()
 }
 
 func (m *Mongo) Create(ctx context.Context, ch chan *mgo.Session, args map[string]interface{}) (interface{}, error) {
-	return zgo_models_mongo.Create(ch, args["db"].(string), args["collection"].(string), args["args"])
+	return zgo_db_mongo.Create(ch, args["db"].(string), args["collection"].(string), args["args"])
 }
 
 func (m *Mongo) Update(ctx context.Context, ch chan *mgo.Session, key string, args map[string]interface{}) (interface{}, error) {
 
-	return nil, zgo_models_mongo.UpdateOne(ch, args["db"].(string), args["collection"].(string),
+	return nil, zgo_db_mongo.UpdateOne(ch, args["db"].(string), args["collection"].(string),
 		args["query"].(bson.M), args["update"].(bson.M))
 }
 
 func (m *Mongo) Delete(ctx context.Context, ch chan *mgo.Session, key string, args map[string]interface{}) (interface{}, error) {
 
-	return nil, zgo_models_mongo.DeleteOne(ch, args["db"].(string), args["collection"].(string),
+	return nil, zgo_db_mongo.DeleteOne(ch, args["db"].(string), args["collection"].(string),
 		args["query"].(bson.M))
 }
 
@@ -44,18 +44,18 @@ func (m *Mongo) List(ctx context.Context, ch chan *mgo.Session, args map[string]
 		args["size"] = 10
 	}
 	if args["limit"] != 0 {
-		return zgo_models_mongo.ListByLimit(ch, args["db"].(string), args["collection"].(string),
+		return zgo_db_mongo.ListByLimit(ch, args["db"].(string), args["collection"].(string),
 			args["from"].(int), args["size"].(int), args["query"].(bson.M), args["select"].(bson.M), sort.([]string))
 	}
-	return zgo_models_mongo.List(ch, args["db"].(string), args["collection"].(string),
+	return zgo_db_mongo.List(ch, args["db"].(string), args["collection"].(string),
 		args["query"].(bson.M))
 }
 
 func (m *Mongo) Get(ctx context.Context, ch chan *mgo.Session, args map[string]interface{}) (chan interface{}, error) {
 	//if args["select"] != nil {
-	//	return zgo_models_mongo.GetBySelect(ch,args["db"].(string), args["collection"].(string),
+	//	return zgo_db_mongo.GetBySelect(ch,args["db"].(string), args["collection"].(string),
 	//		args["query"].(bson.M), args["select"].(bson.M))
 	//}
-	return zgo_models_mongo.Get(ch, args["db"].(string), args["collection"].(string),
+	return zgo_db_mongo.Get(ch, args["db"].(string), args["collection"].(string),
 		args["query"].(bson.M))
 }
