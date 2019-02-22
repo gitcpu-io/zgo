aa1058e77a22033ea3b6cc524f208b84e49b30ee
zhuhonglei <zhuhonglei@zhuge.com>
2019-02-22 17:20:41 +0800

    modify log for zgo.Log

--- logic/zgo_log/log_test.go
+++ logic/zgo_log/log_test.go
@@ -1,16 +1,12 @@
 package zgo_log
 
 import (
-	"github.com/sirupsen/logrus"
-	"golang-bootstrap/lib/log"
 	"testing"
 )
 
-func TestT(t *testing.T) {
-	//nl := T()
-	log.WithFields(logrus.Fields{
-		"animal": "walrus",
-		"size":   10,
-	}).Info("A group of walrus emerges from the ocean")
+func TestZlog_Info(t *testing.T) {
 
+	l := Newzlog()
+	l.InitLog("newProject", "debug")
+	l.Info("33333")
 }
