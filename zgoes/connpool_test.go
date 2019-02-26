1b8ff64275a25f141bd5f4ce4a0a39275c0479f2
jianguo <jianguo@zhugefang.com>
2019-02-26 15:58:02 +0800

    test es

--- zgoes/connpool_test.go
+++ zgoes/connpool_test.go
@@ -14,7 +14,7 @@ const (
 	label_rent = "rents"
 )
 
-func estEsSearch(t *testing.T) {
+func TestEsSearch(t *testing.T) {
 	hsm := make(map[string][]config.ConnDetail)
 	new := config.ConnDetail{
 		C:        "北京主库-----es1",
@@ -66,14 +66,13 @@ func estEsSearch(t *testing.T) {
 		for i := 0;i < 1000 ; i++  {
 			result, err := sellR.Search(ctx, args)
 			if err != nil{
-				fmt.Println(err)
+				//fmt.Println(err)
+				//panic(err)
+				t.Fatal(err)
 			}
 			out <- result
-
 		}
-
 	}()
-
 	//result, err := sellR.Search(ctx, args)
 	for i := 0;i < 1000 ; i++  {
 		fmt.Println(<- out)
@@ -81,59 +80,58 @@ func estEsSearch(t *testing.T) {
 	}
 	finish := time.Now()
 	fmt.Println(finish.Sub(start))
-	//fmt.Print(result, err)
 
 }
 
-func BenchmarkGetEs(b *testing.B) {
-	hsm := make(map[string][]config.ConnDetail)
-	new := config.ConnDetail{
-		C:        "北京主库-----es1",
-		Uri:      "http://101.201.28.195:9200",
-		Host:     "http://101.201.28.195",
-		Port:     9200,
-		ConnSize: 50,
-		PoolSize: 20000,
-	}
-	sell := config.ConnDetail{
-		C:        "北京主库-----es2",
-		Uri:      "http://101.201.28.195:9200",
-		Host:     "http://101.201.28.195",
-		Port:     9200,
-		ConnSize: 50,
-		PoolSize: 20000,
-	}
-	rent := config.ConnDetail{
-		C:        "上海主库-----es",
-		Uri:      "http://101.201.28.195:9200",
-		Host:     "http://101.201.28.195",
-		Port:     9200,
-		ConnSize: 50,
-		PoolSize: 20000,
-	}
-	var s1 []config.ConnDetail
-	var s2 []config.ConnDetail
-	var s3 []config.ConnDetail
-	s1 = append(s1, new)
-	s2 = append(s2, sell)
-	s3 = append(s3, rent)
-	hsm = map[string][]config.ConnDetail{
-		label_new:  s1,
-		label_sell: s2,
-		label_rent: s3,
-	}
-	InitEs(hsm)
-	sellR, _ := GetEs(label_sell)
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-	args := map[string]interface{}{}
-	args["index"] = "active_bj_house_sell"
-	args["table"] = "spider"
-	args["dsl"] = `{"query": {"match_all": {}}}`
-	// 必须循环 b.N 次 。 这个数字 b.N 会在运行中调整，以便最终达到合适的时间消耗。方便计算出合理的数据。 （ 免得数据全部是 0 ）
-	for i:=0 ; i<b.N ; i++ {
-		_, err := sellR.Search(ctx, args)
-		if err != nil{
-			panic(err)
-		}
-	}
-}
+//func BenchmarkGetEs(b *testing.B) {
+//	hsm := make(map[string][]config.ConnDetail)
+//	new := config.ConnDetail{
+//		C:        "北京主库-----es1",
+//		Uri:      "http://101.201.28.195:9200",
+//		Host:     "http://101.201.28.195",
+//		Port:     9200,
+//		ConnSize: 50,
+//		PoolSize: 20000,
+//	}
+//	sell := config.ConnDetail{
+//		C:        "北京主库-----es2",
+//		Uri:      "http://101.201.28.195:9200",
+//		Host:     "http://101.201.28.195",
+//		Port:     9200,
+//		ConnSize: 50,
+//		PoolSize: 20000,
+//	}
+//	rent := config.ConnDetail{
+//		C:        "上海主库-----es",
+//		Uri:      "http://101.201.28.195:9200",
+//		Host:     "http://101.201.28.195",
+//		Port:     9200,
+//		ConnSize: 50,
+//		PoolSize: 20000,
+//	}
+//	var s1 []config.ConnDetail
+//	var s2 []config.ConnDetail
+//	var s3 []config.ConnDetail
+//	s1 = append(s1, new)
+//	s2 = append(s2, sell)
+//	s3 = append(s3, rent)
+//	hsm = map[string][]config.ConnDetail{
+//		label_new:  s1,
+//		label_sell: s2,
+//		label_rent: s3,
+//	}
+//	InitEs(hsm)
+//	sellR, _ := GetEs(label_sell)
+//	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+//	args := map[string]interface{}{}
+//	args["index"] = "active_bj_house_sell"
+//	args["table"] = "spider"
+//	args["dsl"] = `{"query": {"match_all": {}}}`
+//	// 必须循环 b.N 次 。 这个数字 b.N 会在运行中调整，以便最终达到合适的时间消耗。方便计算出合理的数据。 （ 免得数据全部是 0 ）
+//	for i:=0 ; i<b.N ; i++ {
+//		_, err := sellR.Search(ctx, args)
+//		if err != nil{
+//			panic(err)
+//		}
+//	}
+//}
