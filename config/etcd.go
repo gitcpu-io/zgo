47129d6c4289426b8dc2699769d5a043bf4422e1
zhuhonglei <zhuhonglei@zhuge.com>
2019-03-04 16:05:27 +0800

    add etcd.go

---
+++ config/etcd.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"context"
+	"encoding/json"
+	"etcd/clientv3"
+	"fmt"
+	"time"
+)
+
+/*
+@Time : 2019-03-04 15:09
+@Author : rubinus.chu
+@File : etcd
+@project: zgo
+*/
+
+var client *clientv3.Client
+
+func Init() *clientv3.Client {
+	cli, err := CreateClient()
+	if err != nil {
+		return nil
+	}
+	client = cli
+	return client
+}
+
+func InitConfigByEtcd() {
+	cli, err := CreateClient()
+	if err != nil {
+		return
+	}
+	key := "zgo/nsq/nsq_label_bj"
+
+	gr, err := cli.KV.Get(context.TODO(), key, clientv3.WithPrevKV())
+
+	vv := gr.Kvs[0].Value
+	cnd := []ConnDetail{}
+	json.Unmarshal(vv, &cnd)
+
+	fmt.Println(cnd)
+
+	Watcher(cli, key)
+}
+
+func Watcher(client *clientv3.Client, key string) {
+
+	watcher := clientv3.NewWatcher(client)
+	wch := watcher.Watch(context.TODO(), key, clientv3.WithPrevKV())
+	go func() {
+		for {
+			select {
+			case r := <-wch:
+				fmt.Println("----watch key do something---", key)
+				fmt.Printf("%+v %s", r, "\n")
+			}
+		}
+	}()
+}
+
+func CreateClient() (*clientv3.Client, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{
+			"0.0.0.0:2381",
+		},
+		DialTimeout: 10 * time.Second,
+	})
+	return cli, err
+}
