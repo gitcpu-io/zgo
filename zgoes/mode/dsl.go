2af8bd1f6c3d515a3b967cd9d53e236c0dc1cbe2
wangshibao <wangshibao@zhugefang.com>
2019-03-11 16:24:57 +0800

    dst_0

--- zgoes/mode/dsl.go
+++ zgoes/mode/dsl.go
@@ -6,21 +6,63 @@ var DSL = map[string]string{
 	"bool": `{"source":"{\"query\":{\"bool\":{\"must\":{{#toJson}}must{{/toJson}},\"should\":{{#toJson}}should{{/toJson}},\"must_not\":{{#toJson}}must_not{{/toJson}} }}}}","params":{"must":%s,"should":%s,"must_not":%s}}`,
 }
 
+var SimpleDsl = map[string]interface{}{
+	"source": `{\"query\": {{#toJson}}simple{{/toJson}}}`,
+}
+
 var Term = func(m map[string]interface{}) interface{} {
 	term := map[string]interface{}{}
 	term["term"] = m
-	t, _ := zgoutils.Utils.Marshal(term)
-	return t
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
 }
-var Rang = func(f string, m map[string]interface{}) interface{} {
+var Range = func(f string, m map[string]interface{}) interface{} {
 	term := map[string]interface{}{}
-	term[f] = m
-	t, _ := zgoutils.Utils.Marshal(term)
-	return t
+	term["range"] = map[string]interface{}{
+		f: m,
+	}
+
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
 }
+
 var Match = func(m map[string]interface{}) interface{} {
 	term := map[string]interface{}{}
-	term["term"] = m
-	t, _ := zgoutils.Utils.Marshal(term)
-	return t
+	term["match"] = m
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
+}
+
+var MatchPhrase = func(m map[string]interface{}) interface{} {
+	term := map[string]interface{}{}
+	term["match_phrase"] = m
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
+}
+
+var Wildcard = func(m map[string]interface{}) interface{} {
+	term := map[string]interface{}{}
+	term["wildcard"] = m
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
+}
+
+var GeoBox = func(f string, top_left map[string]interface{},
+	bottom_right map[string]interface{}) interface{} {
+
+	term := map[string]interface{}{}
+	term["geo_bounding_box"] = map[string]interface{}{
+		f: map[string]interface{}{
+			"top_left":     top_left,
+			"bottom_right": bottom_right,
+		},
+	}
+	return term
+	//t, _ := zgoutils.Utils.Marshal(term)
+	//return t
 }
--- zgoes/mode/dsl_test.go
+++ zgoes/mode/dsl_test.go
@@ -5,11 +5,24 @@ import (
 	"testing"
 )
 
-func TestA(t *testing.T) {
+func TestRang(t *testing.T) {
 	//ps := map[string]interface{}{"type_name": "小区"}
 	rg := map[string]interface{}{"gte": 10, "lte": 20}
 
-	a := fmt.Sprintf(DSL["bool"], Rang("city", rg), "[]", "[]")
+	a := fmt.Sprintf(DSL["bool"], Range("city", rg), "[]", "[]")
 
 	fmt.Println(a)
+}
+
+func TestMatch(t *testing.T) {
+	mch := map[string]interface{}{"cityarea2": "朝阳"}
+	a := fmt.Sprintf(DSL["bool"], Match(mch), "[]", "[]")
+
+	fmt.Println(a, string(Match(mch).([]byte)))
+}
+
+func TestMulti(t *testing.T) {
+	rg := map[string]interface{}{"gte": 10, "lte": 20}
+	mch := map[string]interface{}{"cityarea2": "朝阳"}
+
 }
