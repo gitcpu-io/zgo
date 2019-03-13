f4610521f0ac37bc1a42663b444d39f54329873e
wangshibao <wangshibao@zhugefang.com>
2019-03-13 12:35:32 +0800

    dsl_simple

--- zgoes/mode/dsl_test.go
+++ zgoes/mode/dsl_test.go
@@ -2,27 +2,76 @@ package mode
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+func show(T interface{}) {
+	fmt.Println(T, reflect.TypeOf(T))
+}
 
 func TestRang(t *testing.T) {
 	//ps := map[string]interface{}{"type_name": "小区"}
 	rg := map[string]interface{}{"gte": 10, "lte": 20}
 
-	a := fmt.Sprintf(DSL["bool"], Range("city", rg), "[]", "[]")
+	a := fmt.Sprintf(DSL["bool"], unmarshal(RangeMap("city", rg)), "[]", "[]")
 
 	fmt.Println(a)
 }
 
 func TestMatch(t *testing.T) {
 	mch := map[string]interface{}{"cityarea2": "朝阳"}
-	a := fmt.Sprintf(DSL["bool"], Match(mch), "[]", "[]")
+	a := fmt.Sprintf(DSL["bool"], unmarshal(MatchMap(mch)), "[]", "[]")
 
-	fmt.Println(a, string(Match(mch).([]byte)))
+	fmt.Println(a, MatchMap(mch))
 }
 
-func TestMulti(t *testing.T) {
-	rg := map[string]interface{}{"gte": 10, "lte": 20}
-	mch := map[string]interface{}{"cityarea2": "朝阳"}
+func TestSimpleAggs(t *testing.T) {
+	res := SimpleAggs("cityarea_id", 20)
+	show(res)
+}
 
+func TestAggs2Field(t *testing.T) {
+	res := Aggs2Field("cityarea_id", 5, "borough_id", 10)
+	show(fmt.Sprintf(`{"aggs": %s}`, res))
+}
+
+func TestQueryDsl(t *testing.T) {
+	fmt.Println("#########################QueryDsl")
+	must := make([]interface{}, 0)
+	should := make([]interface{}, 0)
+	filter := make([]interface{}, 0)
+	must_not := make([]interface{}, 0)
+
+	var res interface{}
+	res = TermField("cityarea_id", 5)
+	show(res)
+	must = append(must, res)
+	res = MatchPhraseField("cityarea_name", "朝阳")
+	should = append(should, res)
+
+	res = TermField("borough_id", 10)
+	must_not = append(must_not, res)
+
+	res = RangeField("cityarea_id", "lt", 200, "gte", 10)
+	show(res)
+	filter = append(filter, res)
+
+	aggs := SimpleAggs("cityarea_id", 5)
+	sort := SimpleSort("borough_id", true)
+
+	resMap := make(map[string]interface{})
+	resMap["must"] = must
+	resMap["should"] = should
+	resMap["must_not"] = must_not
+	resMap["filter"] = filter
+
+	resMap["sort"] = sort
+	resMap["aggs"] = aggs
+	resMap["from"] = 10
+	resMap["size"] = 100
+	resMap["_source"] = []string{"_id", "cityarea_id"}
+
+	dsl := QueryDsl(resMap)
+	show(dsl)
 }
