ada8c8d27dab0a23cac0ed040c9827af87ae5756
lutaixiang <lutaixiang@zhugefang.com>
2019-03-22 11:17:49 +0800

    修改zgo http 跨域

--- zgohttp/index.go
+++ zgohttp/index.go
@@ -35,36 +35,57 @@ var (
 )
 
 func (zh *zgohttp) JsonpOK(ctx iris.Context, r interface{}) (int, error) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSONP(iris.Map{"code": 200, "data": r, "message": ""})
 }
 
 func (zh *zgohttp) JsonpErr(ctx iris.Context, msg string) (int, error) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSONP(iris.Map{"code": 400, "message": msg, "data": make(map[string]interface{})})
 }
 
 // JsonOK 正常的返回方法
 func (zh *zgohttp) JsonOK(ctx iris.Context, r interface{}) (int, error) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSON(iris.Map{"code": 200, "data": r, "message": ""})
 }
 
 // JsonExpectErr 预期内的错误，适用于调用func后 return出来的errors!=nil时的返回值
 func (zh *zgohttp) JsonExpectErr(ctx iris.Context, msg string) (int, error) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSON(ErrResponse{Status: 500, Msg: msg, ErrorCode: "500", Data: make(map[string]interface{})})
 }
 
 // JsonOtherErr 其他自定义返回方法 （业务本身的异常）
 func (zh *zgohttp) JsonErr(ctx iris.Context, status int, code string, msg string) (int, error) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSON(ErrResponse{Status: status, Msg: msg, ErrorCode: code})
 }
 
 // JsonServiceErr defer recover到panic的时候用的异常方法
 func (zh *zgohttp) JsonServiceErr(ctx iris.Context) (int, error) {
 	msg := "服务器开小差了，稍后再试吧"
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSON(ErrResponse{Status: 500, Msg: msg, ErrorCode: "500", Data: make(map[string]interface{})})
 }
 
 // JsonParamErr 参数验证不通过时调用
 func (zh *zgohttp) JsonParamErr(ctx iris.Context) (int, error) {
 	msg := "参数错误"
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+	ctx.Header("content-type", "application/json")
 	return ctx.JSON(ErrResponse{Status: 400, Msg: msg, ErrorCode: "400", Data: make(map[string]interface{})})
 }
