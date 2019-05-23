e550364f77141e91c2be41aab1bbae8feecaec7c
zhuhonglei <zhuhonglei@zhuge.com>
2019-05-23 14:25:06 +0800

    v0.9.8 添加bucket

--- limiter/simple.go
+++ limiter/simple.go
@@ -8,9 +8,10 @@ package limiter
 */
 
 type SimpleBucketer interface {
-	GetToken() int32
-	ReleaseToken()
+	GetToken(num int32) int32
+	ReleaseToken(num int32) int32
 	Len() int32
+	Capacity() int32
 	Reset(size int32)
 }
 
@@ -22,33 +23,59 @@ type SimpleBucket struct {
 func NewSimpleBucket(cc int32) *SimpleBucket {
 	return &SimpleBucket{
 		concurrent: cc,
-		bucket:     make(chan uint8, cc),
+		bucket:     make(chan uint8, 999999999),
 	}
 }
 
-func (cl *SimpleBucket) GetToken() int32 {
-	if cl.Len() >= cl.concurrent {
+// GetToken 提取指定num个token，如果不够返回真实可用的token个数
+func (cl *SimpleBucket) GetToken(num int32) int32 {
+
+	cLen := cl.Len()
+
+	if cLen >= cl.concurrent {
 		return 0
 	}
-	cl.bucket <- 1
-	return cl.Len()
+
+	beef := cl.concurrent - cLen
+	if beef < num { //没有足够的token可以提取
+		num = beef
+	}
+	for i := 0; int32(i) < num; i++ {
+		cl.bucket <- 1
+	}
+	return num
 }
 
-func (cl *SimpleBucket) ReleaseToken() {
-	<-cl.bucket
+// ReleaseToken 释放指定个num的token，如果释放太多，按真实的可释放数
+func (cl *SimpleBucket) ReleaseToken(num int32) int32 {
+	beef := cl.Len()
+	if beef < num {
+		num = beef
+	}
+	for i := 0; int32(i) < num; i++ {
+		<-cl.bucket
+	}
+	return num
 }
 
 func (cl *SimpleBucket) Len() int32 {
 	return int32(len(cl.bucket))
 }
 
+func (cl *SimpleBucket) Capacity() int32 {
+	return cl.concurrent
+}
+
 func (cl *SimpleBucket) Reset(size int32) {
-	if cl.Len() == size {
-		return //当传进来的size和bucket长度一样时，不需要重置大小
+	if cl.concurrent == size {
+		return //当传进来的size和原来长度一样时，不需要重置大小
 	}
-	if cl.Len() > size { //如果减小，就从循环从chan中读取offset
-		for i := 0; i < int(cl.Len()-size); i++ {
-			cl.ReleaseToken()
+
+	if cl.concurrent > size { //如果减小，就从循环从chan中读取offset
+
+		beef := cl.Len() - size
+		if beef > 0 {
+			cl.ReleaseToken(beef)
 		}
 	}
 	cl.concurrent = size //变大或变小时对并发量进行重置
