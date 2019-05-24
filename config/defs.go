6f0543274d7728e2e205a6c68b49d28e82eafe02
zhuhonglei <zhuhonglei@zhuge.com>
2019-05-24 18:16:11 +0800

    v0.9. 添加bucket

--- config/defs.go
+++ config/defs.go
@@ -21,7 +21,7 @@ const (
 	Warn         //2
 	Error        //3
 
-	Version       = "0.9.8"       //zgo版本号
+	Version       = "0.9.9"       //zgo版本号
 	ProjectPrefix = "zgo/project" //读取ETCD配置时prefix
 	FileStoreType = "local"       //文件存储类型
 	FileStoreHome = "/tmp"        //文件存储目录
--- limiter/simple.go
+++ limiter/simple.go
@@ -16,7 +16,9 @@ type SimpleBucketer interface {
 	Release(num int32) int32
 	Len() int32
 	Cap() int32
+	BeLeft() int32
 	Resize(size int32)
+	Clear() int32
 }
 
 type SimpleBucket struct {
@@ -32,6 +34,7 @@ func NewBucket(cc int32) *SimpleBucket {
 	return &SimpleBucket{
 		capacity: cc,
 		bucket:   make(chan uint8, max),
+		offset:   0,
 	}
 }
 
@@ -42,6 +45,11 @@ func (cl *SimpleBucket) Get(num int32) int32 {
 	}
 
 	cLen := cl.Len()
+
+	if cl.offset != 0 && cLen > cl.offset {
+		//fmt.Println(cl.offset, "-------offset有值，返回不了")
+		return 0
+	}
 
 	if cLen >= cl.capacity {
 		return 0
@@ -63,9 +71,9 @@ func (cl *SimpleBucket) Release(num int32) int32 {
 		return -1
 	}
 
-	if cl.offset > 0 {
-		cl.offset--
-		return -1 //resize时过滤掉偏移量
+	if cl.offset != 0 && cl.offset-num >= 0 {
+		cl.offset -= num
+		//fmt.Println(cl.offset, "=========cl.offset==========")
 	}
 
 	beef := cl.Len()
@@ -77,38 +85,77 @@ func (cl *SimpleBucket) Release(num int32) int32 {
 	if beef < num {
 		num = beef
 	}
+
 	for i := 0; int32(i) < num; i++ {
 		<-cl.bucket
 	}
 	return num
 }
 
+// Clear 清空整个桶
+func (cl *SimpleBucket) Clear() int32 {
+	cl.offset = 0
+	cLen := cl.Len()
+	for i := 0; int32(i) < cLen; i++ {
+		<-cl.bucket
+	}
+	return cLen
+}
+
+// Len 已经使用的长度
 func (cl *SimpleBucket) Len() int32 {
 	return int32(len(cl.bucket))
 }
 
+// Cap 整个桶的容量
 func (cl *SimpleBucket) Cap() int32 {
 	return cl.capacity
 }
 
+// BeLeft 剩余可以使用的
+func (cl *SimpleBucket) BeLeft() int32 {
+	return cl.Cap() - cl.Len()
+}
+
 func (cl *SimpleBucket) Resize(size int32) {
-	if cl.capacity == size {
+	if cl.capacity == size || size == 0 {
 		return //当传进来的size和原来长度一样时，不需要重置大小
 	}
 
-	if cl.capacity > size { //如果减小
+	oldCapacity := cl.capacity
+
+	cl.capacity = size
+
+	if oldCapacity > size { //如果减小
+		offset := oldCapacity - size
+
 		cLen := cl.Len()
-		beef := cLen - size
-		if beef >= 0 { //有足够的token可以释放
-			cl.Release(cl.capacity - size)
-		} else if beef < 0 { //比较麻烦，表明token已全部或部分释放掉，正在回来的路上，属于中间态
+
+		if cLen-offset >= 0 { //桶中有足够的token可以释放
+			//fmt.Println("--有足够的token可以释放--",cLen, offset,cl.capacity)
+
+			cl.Release(offset)
+
+			cl.offset = offset
+
+		} else { //比较麻烦，表明token已全部或部分释放掉，正在回来的路上，属于中间态
+
 			if cLen != 0 {
-				cl.offset = size - cLen //还有中间态未释放掉
-				cl.Release(cLen)        //释放掉部分
+
+				nw := cl.Release(cLen) //释放掉部分
+
+				cl.offset = offset - nw
+
+				//fmt.Println("释放已有token:",nw, cl.offset,"----------------000000000===",offset, cLen,oldCapacity, cl.capacity, size)
+
 			} else {
-				cl.offset = size
+				cl.offset = offset
+
+				//fmt.Println(cl.offset,"----------------111111111===",offset, cLen,oldCapacity, cl.capacity, size)
+
 			}
+
 		}
+
 	}
-	cl.capacity = size //变大或变小时对并发量进行重置
 }
