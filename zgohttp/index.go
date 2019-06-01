06c3b6ed5e029aa3f7eeb9c4b75f5c7f1764d570
lucas <409695381@qq.com>
2019-06-01 21:54:26 +0800

    add count join

--- zgohttp/index.go
+++ zgohttp/index.go
@@ -3,8 +3,21 @@ package zgohttp
 import (
 	"fmt"
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+	"runtime"
+	"strconv"
 	"time"
 )
+
+func getRequestLogs(ctx context.Context) string {
+	var status, ip, method, path string
+	status = strconv.Itoa(ctx.GetStatusCode())
+	path = ctx.Path()
+	method = ctx.Method()
+	ip = ctx.RemoteAddr()
+	// the date should be logged by iris' Logger, so we skip them
+	return fmt.Sprintf("%v %s %s %s", status, path, method, ip)
+}
 
 type Httper interface {
 	JsonpOK(ctx iris.Context, r interface{}) (int, error)
@@ -112,8 +125,27 @@ func (zh *zgohttp) JsonParamErr(ctx iris.Context) (int, error) {
 func (zh *zgohttp) UseBefore(ctx iris.Context) {
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println(err)
+			//fmt.Println(err)
 			zh.JsonServiceErr(ctx)
+			if ctx.IsStopped() {
+				return
+			}
+			var stacktrace string
+			for i := 1; ; i++ {
+				_, f, l, got := runtime.Caller(i)
+				if !got {
+					break
+				}
+				stacktrace += fmt.Sprintf("%s:%d\n", f, l)
+			}
+			// when stack finishes
+			logMessage := fmt.Sprintf("Recovered from a route's Handler('%s')\n", ctx.HandlerName())
+			logMessage += fmt.Sprintf("At Request: %s\n", getRequestLogs(ctx))
+			logMessage += fmt.Sprintf("Trace: %s\n", err)
+			logMessage += fmt.Sprintf("\n%s", stacktrace)
+			ctx.Application().Logger().Warn(logMessage)
+			//ctx.StatusCode(500)
+			ctx.StopExecution()
 		}
 	}()
 	start := time.Now().UnixNano()
