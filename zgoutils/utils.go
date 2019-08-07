ee8e011e3ca61defe04435fa108701890bc2c5e2
朱大仙儿 <zhuhonglei@zhuge.com>
2019-08-07 14:37:09 +0800

    添加ip to number

--- zgoutils/utils.go
+++ zgoutils/utils.go
@@ -107,6 +107,10 @@ type Utilser interface {
 	Extract(addr string) (string, error)
 	//获取内网ip
 	GetIntranetIP() string
+	//数字转ip地址
+	UInt32ToIP(intIP uint32) net.IP
+	//ip地址转数字
+	IPToUInt32(ipnr net.IP) uint32
 
 	GetUUIDV4() string
 	GetMD5Base64([]byte) string
@@ -525,6 +529,36 @@ func (u *utils) GetIntranetIP() string {
 	}
 
 	return ip_cache
+}
+
+// IPToUInt32
+func (u *utils) IPToUInt32(ipnr net.IP) uint32 {
+	bits := strings.Split(ipnr.String(), ".")
+
+	b0, _ := strconv.Atoi(bits[0])
+	b1, _ := strconv.Atoi(bits[1])
+	b2, _ := strconv.Atoi(bits[2])
+	b3, _ := strconv.Atoi(bits[3])
+
+	var sum uint32
+
+	sum += uint32(b0) << 24
+	sum += uint32(b1) << 16
+	sum += uint32(b2) << 8
+	sum += uint32(b3)
+
+	return sum
+}
+
+// UInt32ToIP
+func (u *utils) UInt32ToIP(intIP uint32) net.IP {
+	var bytes [4]byte
+	bytes[0] = byte(intIP & 0xFF)
+	bytes[1] = byte((intIP >> 8) & 0xFF)
+	bytes[2] = byte((intIP >> 16) & 0xFF)
+	bytes[3] = byte((intIP >> 24) & 0xFF)
+
+	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0])
 }
 
 //ParseDns
