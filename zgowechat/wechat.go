48df149b43e24b83500fd5584470678f77063967
朱大仙儿 <zhuhonglei@zhuge.com>
2019-10-16 13:44:19 +0800

    update ali and wechat pay func

--- zgowechat/wechat.go
+++ zgowechat/wechat.go
@@ -9,9 +9,10 @@ package zgowechat
 
 type Wechat interface {
 	Pay(appId, mchId, apiKey string, isProd bool) Payer
+	//返回微信通知NotifyPayResponse
+	NotifyPayResponse(code, msg string) string
 
 	//添加其它接口
-
 }
 
 type wechat struct {
@@ -19,6 +20,15 @@ type wechat struct {
 
 func (w *wechat) Pay(appId, mchId, apiKey string, isProd bool) Payer {
 	return NewPayClient(appId, mchId, apiKey, isProd)
+}
+
+//返回微信通知NotifyPayResponse
+func (w *wechat) NotifyPayResponse(code, msg string) string {
+	nr := &NotifyResponse{
+		ReturnCode: code,
+		ReturnMsg:  msg,
+	}
+	return nr.ToXmlString()
 }
 
 func New() Wechat {
